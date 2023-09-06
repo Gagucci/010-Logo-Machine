e9d81b543f1f49d531124f195a32906b33fd0e79
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-09-06 00:43:51 -0700

    added test parameters and set properties for shape

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { circle, square, triangle } = require("./libs/shapes");
+
+class Logo {
+  constructor() {
+    this.textEl = "";
+    this.shapeEl = "";
+  }
+}
