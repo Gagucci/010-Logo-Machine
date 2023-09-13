c0a9714a54e2a90c093f5372b6aaa64e081a48c4
Gagucci <126989750+Gagucci@users.noreply.github.com>
2023-09-13 16:55:18 -0700

    made text render correctlt & tests all pass

--- index.js
+++ index.js
@@ -5,23 +5,34 @@ const { Circle, Square, Triangle } = require("./lib/shapes");
 class Logo {
   constructor() {
     this.textEl = "";
-    this.shapeEl = "";
+    this.shapeEl = answers.shape;
   }
   render() {
-    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 300 200" preserveAspectRatio="xMidYMid meet">' +
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 300 200" preserveAspectRatio="xMidYMid meet">
       ${this.shapeEl} ${this.textEl} 
-      </svg>`
+      </svg>`;
   }
-  addTextEl(text, textColor) {
-    this.textEl = `
-    <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-family="sans-serif" font-size="25px">${text}</text>
-    `;
+
+  addTextEl(text, textColor, shape) {
+    if (shape === "Circle") {
+      this.textEl += `
+        <text x="33%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-family="sans-serif" font-size="50px">${text}</text>
+        `;
+    } else if (shape === "Square") {
+      this.textEl += `
+        <text x="125" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-family="sans-serif" font-size="50px">${text}</text>
+        `;
+    } else if (shape === "Triangle") {
+      this.textEl += `
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-family="sans-serif" font-size="50px">${text}</text>
+        `;
+    }
+    return;
   }
-  // uses specified shape to render
   addShapeEl(shape) {
     this.shapeEl += shape.render();
-  };
-};
+  }
+}
 
 // prompt for user input on logo parameters
 const logoPrompt = [
@@ -48,11 +59,13 @@ const logoPrompt = [
   },
 ];
 
+let answers;
+
 // function to prompt logo creation and creation of logo.svg file
 function init() {
-  inquirer.prompt(logoPrompt).then((answers) => {
+  inquirer.prompt(logoPrompt).then((userAnswers) => {
+    answers = userAnswers;
     const logo = new Logo();
-    logo.addTextEl(answers.text, answers.textColor);
     switch (answers.shape) {
       case "Circle":
         const circle = new Circle();
@@ -70,9 +83,12 @@ function init() {
         logo.addShapeEl(triangle);
         break;
     }
+    logo.addTextEl(answers.text, answers.textColor, answers.shape);
+
     fs.writeFile("logo.svg", logo.render(), (err) => {
       if (err) throw err;
       console.log("Logo created!");
+      console.log(answers);
     });
   });
 }
